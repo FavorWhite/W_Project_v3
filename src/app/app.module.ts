7fbc41a17e3415384da0f719e24b4cfc30121da5
FavorWhite <bilyi.valery@gmail.com>
2023-10-29 19:27:57 +0200

    Separate in two side , and Location search comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationSearchComponent } from './location-search/location-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LocationSearchComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
